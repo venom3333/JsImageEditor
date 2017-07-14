389ed86d0f08889c678773e6851b8de4366f92ee
Vladimir Venom <venom3333@gmail.com>
2017-07-14 14:13:03 +0300

    First Commit

---
+++ js/core/darkroom.js
@@ -0,0 +1,366 @@
+/* global fabric */
+
+!function() {
+    "use strict";
+    var element = document.createElement("div");
+    element.id = "darkroom-icons",
+    element.style.height = 0,
+    element.style.width = 0,
+    element.style.position = "absolute",
+    element.style.visibility = "hidden",
+    element.innerHTML = '<!-- inject:svg --><svg xmlns="http://www.w3.org/2000/svg"><symbol id="close" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></symbol><symbol id="crop" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 15h2V7c0-1.1-.9-2-2-2H9v2h8v8zM7 17V1H5v4H1v2h4v10c0 1.1.9 2 2 2h10v4h2v-4h4v-2H7z"/></symbol><symbol id="done" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></symbol><symbol id="redo" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z"/></symbol><symbol id="rotate-left" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7.11 8.53L5.7 7.11C4.8 8.27 4.24 9.61 4.07 11h2.02c.14-.87.49-1.72 1.02-2.47zM6.09 13H4.07c.17 1.39.72 2.73 1.62 3.89l1.41-1.42c-.52-.75-.87-1.59-1.01-2.47zm1.01 5.32c1.16.9 2.51 1.44 3.9 1.61V17.9c-.87-.15-1.71-.49-2.46-1.03L7.1 18.32zM13 4.07V1L8.45 5.55 13 10V6.09c2.84.48 5 2.94 5 5.91s-2.16 5.43-5 5.91v2.02c3.95-.49 7-3.85 7-7.93s-3.05-7.44-7-7.93z"/></symbol><symbol id="rotate-right" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.55 5.55L11 1v3.07C7.06 4.56 4 7.92 4 12s3.05 7.44 7 7.93v-2.02c-2.84-.48-5-2.94-5-5.91s2.16-5.43 5-5.91V10l4.55-4.45zM19.93 11c-.17-1.39-.72-2.73-1.62-3.89l-1.42 1.42c.54.75.88 1.6 1.02 2.47h2.02zM13 17.9v2.02c1.39-.17 2.74-.71 3.9-1.61l-1.44-1.44c-.75.54-1.59.89-2.46 1.03zm3.89-2.42l1.42 1.41c.9-1.16 1.45-2.5 1.62-3.89h-2.02c-.14.87-.48 1.72-1.02 2.48z"/></symbol><symbol id="save" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></symbol><symbol id="undo" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z"/></symbol></svg><!-- endinject -->',
+    document.body.appendChild(element);
+}(),
+(function() {
+'use strict';
+
+window.Darkroom = Darkroom;
+
+// Core object of DarkroomJS.
+// Basically it's a single object, instanciable via an element
+// (it could be a CSS selector or a DOM element), some custom options,
+// and a list of plugin objects (or none to use default ones).
+function Darkroom(element, options, plugins) {
+  return this.constructor(element, options, plugins);
+}
+
+// Create an empty list of plugin objects, which will be filled by
+// other plugin scripts. This is the default plugin list if none is
+// specified in Darkroom'ss constructor.
+Darkroom.plugins = [];
+
+Darkroom.prototype = {
+  // Reference to the main container element
+  containerElement: null,
+
+  // Reference to the Fabric canvas object
+  canvas: null,
+
+  // Reference to the Fabric image object
+  image: null,
+
+  // Reference to the Fabric source canvas object
+  sourceCanvas: null,
+
+  // Reference to the Fabric source image object
+  sourceImage: null,
+
+  // Track of the original image element
+  originalImageElement: null,
+
+  // Stack of transformations to apply to the image source
+  transformations: [],
+
+  // Default options
+  defaults: {
+    // Canvas properties (dimension, ratio, color)
+    minWidth: null,
+    minHeight: null,
+    maxWidth: null,
+    maxHeight: null,
+    ratio: null,
+    backgroundColor: '#fff',
+
+    // Plugins options
+    plugins: {},
+
+    // Post-initialisation callback
+    initialize: function() { /* noop */ }
+  },
+
+  // List of the instancied plugins
+  plugins: {},
+
+  // This options are a merge between `defaults` and the options passed
+  // through the constructor
+  options: {},
+
+  constructor: function(element, options, plugins) {
+    this.options = Darkroom.Utils.extend(options, this.defaults);
+
+    if (typeof element === 'string')
+      element = document.querySelector(element);
+    if (null === element)
+      return;
+
+    var image = new Image();
+    image.onload = function() {
+      // Initialize the DOM/Fabric elements
+      this._initializeDOM(element);
+      this._initializeImage();
+
+      // Then initialize the plugins
+      this._initializePlugins(Darkroom.plugins);
+
+      // Public method to adjust image according to the canvas
+      this.refresh(function() {
+        // Execute a custom callback after initialization
+        this.options.initialize.bind(this).call();
+      }.bind(this));
+
+    }.bind(this);
+
+    //image.crossOrigin = 'anonymous';
+    image.src = element.src;
+  },
+
+  selfDestroy: function() {
+    var container = this.containerElement;
+    var image = new Image();
+    image.onload = function() {
+      container.parentNode.replaceChild(image, container);
+    };
+
+    image.src = this.sourceImage.toDataURL();
+  },
+
+  // Add ability to attach event listener on the core object.
+  // It uses the canvas element to process events.
+  addEventListener: function(eventName, callback) {
+    var el = this.canvas.getElement();
+    if (el.addEventListener){
+      el.addEventListener(eventName, callback);
+    } else if (el.attachEvent) {
+      el.attachEvent('on' + eventName, callback);
+    }
+  },
+
+  dispatchEvent: function(eventName) {
+    // Use the old way of creating event to be IE compatible
+    // See https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
+    var event = document.createEvent('Event');
+    event.initEvent(eventName, true, true);
+
+    this.canvas.getElement().dispatchEvent(event);
+  },
+
+  // Adjust image & canvas dimension according to min/max width/height
+  // and ratio specified in the options.
+  // This method should be called after each image transformation.
+  refresh: function(next) {
+    var clone = new Image();
+    clone.onload = function() {
+      this._replaceCurrentImage(new fabric.Image(clone));
+
+      if (next) next();
+    }.bind(this);
+    clone.src = this.sourceImage.toDataURL();
+  },
+
+  _replaceCurrentImage: function(newImage) {
+    if (this.image) {
+      this.image.remove();
+    }
+
+    this.image = newImage;
+    this.image.selectable = false;
+
+    // Adjust width or height according to specified ratio
+    var viewport = Darkroom.Utils.computeImageViewPort(this.image);
+    var canvasWidth = viewport.width;
+    var canvasHeight = viewport.height;
+
+    if (null !== this.options.ratio) {
+      var canvasRatio = +this.options.ratio;
+      var currentRatio = canvasWidth / canvasHeight;
+
+      if (currentRatio > canvasRatio) {
+        canvasHeight = canvasWidth / canvasRatio;
+      } else if (currentRatio < canvasRatio) {
+        canvasWidth = canvasHeight * canvasRatio;
+      }
+    }
+
+    // Then scale the image to fit into dimension limits
+    var scaleMin = 1;
+    var scaleMax = 1;
+    var scaleX = 1;
+    var scaleY = 1;
+
+    if (null !== this.options.maxWidth && this.options.maxWidth < canvasWidth) {
+      scaleX =  this.options.maxWidth / canvasWidth;
+    }
+    if (null !== this.options.maxHeight && this.options.maxHeight < canvasHeight) {
+      scaleY =  this.options.maxHeight / canvasHeight;
+    }
+    scaleMin = Math.min(scaleX, scaleY);
+
+    scaleX = 1;
+    scaleY = 1;
+    if (null !== this.options.minWidth && this.options.minWidth > canvasWidth) {
+      scaleX =  this.options.minWidth / canvasWidth;
+    }
+    if (null !== this.options.minHeight && this.options.minHeight > canvasHeight) {
+      scaleY =  this.options.minHeight / canvasHeight;
+    }
+    scaleMax = Math.max(scaleX, scaleY);
+
+    var scale = scaleMax * scaleMin; // one should be equals to 1
+
+    canvasWidth *= scale;
+    canvasHeight *= scale;
+
+    // Finally place the image in the center of the canvas
+    this.image.setScaleX(1 * scale);
+    this.image.setScaleY(1 * scale);
+    this.canvas.add(this.image);
+    this.canvas.setWidth(canvasWidth);
+    this.canvas.setHeight(canvasHeight);
+    this.canvas.centerObject(this.image);
+    this.image.setCoords();
+  },
+
+  // Apply the transformation on the current image and save it in the
+  // transformations stack (in order to reconstitute the previous states
+  // of the image).
+  applyTransformation: function(transformation) {
+    this.transformations.push(transformation);
+
+    transformation.applyTransformation(
+      this.sourceCanvas,
+      this.sourceImage,
+      this._postTransformation.bind(this)
+    );
+  },
+
+  _postTransformation: function(newImage) {
+    if (newImage)
+      this.sourceImage = newImage;
+
+    this.refresh(function() {
+      this.dispatchEvent('core:transformation');
+    }.bind(this));
+  },
+
+  // Initialize image from original element plus re-apply every
+  // transformations.
+  reinitializeImage: function() {
+    this.sourceImage.remove();
+    this._initializeImage();
+    this._popTransformation(this.transformations.slice());
+  },
+
+  _popTransformation: function(transformations) {
+    if (0 === transformations.length) {
+      this.dispatchEvent('core:reinitialized');
+      this.refresh();
+      return;
+    }
+
+    var transformation = transformations.shift();
+
+    var next = function(newImage) {
+      if (newImage) this.sourceImage = newImage;
+      this._popTransformation(transformations);
+    };
+
+    transformation.applyTransformation(
+      this.sourceCanvas,
+      this.sourceImage,
+      next.bind(this)
+    );
+  },
+
+  // Create the DOM elements and instanciate the Fabric canvas.
+  // The image element is replaced by a new `div` element.
+  // However the original image is re-injected in order to keep a trace of it.
+  _initializeDOM: function(imageElement) {
+    // Container
+    var mainContainerElement = document.createElement('div');
+    mainContainerElement.className = 'darkroom-container';
+
+    // Toolbar
+    var toolbarElement = document.createElement('div');
+    toolbarElement.className = 'darkroom-toolbar';
+    mainContainerElement.appendChild(toolbarElement);
+
+    // Viewport canvas
+    var canvasContainerElement = document.createElement('div');
+    canvasContainerElement.className = 'darkroom-image-container';
+    var canvasElement = document.createElement('canvas');
+    canvasContainerElement.appendChild(canvasElement);
+    mainContainerElement.appendChild(canvasContainerElement);
+
+    // Source canvas
+    var sourceCanvasContainerElement = document.createElement('div');
+    sourceCanvasContainerElement.className = 'darkroom-source-container';
+    sourceCanvasContainerElement.style.display = 'none';
+    var sourceCanvasElement = document.createElement('canvas');
+    sourceCanvasContainerElement.appendChild(sourceCanvasElement);
+    mainContainerElement.appendChild(sourceCanvasContainerElement);
+
+    // Original image
+    imageElement.parentNode.replaceChild(mainContainerElement, imageElement);
+    imageElement.style.display = 'none';
+    mainContainerElement.appendChild(imageElement);
+
+    // Instanciate object from elements
+    this.containerElement = mainContainerElement;
+    this.originalImageElement = imageElement;
+
+    this.toolbar = new Darkroom.UI.Toolbar(toolbarElement);
+
+    this.canvas = new fabric.Canvas(canvasElement, {
+      selection: false,
+      backgroundColor: this.options.backgroundColor
+    });
+
+    this.sourceCanvas = new fabric.Canvas(sourceCanvasElement, {
+      selection: false,
+      backgroundColor: this.options.backgroundColor
+    });
+  },
+
+  // Instanciate the Fabric image object.
+  // The image is created as a static element with no control,
+  // then it is add in the Fabric canvas object.
+  _initializeImage: function() {
+    this.sourceImage = new fabric.Image(this.originalImageElement, {
+      // Some options to make the image static
+      selectable: false,
+      evented: false,
+      lockMovementX: true,
+      lockMovementY: true,
+      lockRotation: true,
+      lockScalingX: true,
+      lockScalingY: true,
+      lockUniScaling: true,
+      hasControls: false,
+      hasBorders: false
+    });
+
+    this.sourceCanvas.add(this.sourceImage);
+
+    // Adjust width or height according to specified ratio
+    var viewport = Darkroom.Utils.computeImageViewPort(this.sourceImage);
+    var canvasWidth = viewport.width;
+    var canvasHeight = viewport.height;
+
+    this.sourceCanvas.setWidth(canvasWidth);
+    this.sourceCanvas.setHeight(canvasHeight);
+    this.sourceCanvas.centerObject(this.sourceImage);
+    this.sourceImage.setCoords();
+  },
+
+  // Initialize every plugins.
+  // Note that plugins are instanciated in the same order than they
+  // are declared in the parameter object.
+  _initializePlugins: function(plugins) {
+    for (var name in plugins) {
+      var plugin = plugins[name];
+      var options = this.options.plugins[name];
+
+      // Setting false into the plugin options will disable the plugin
+      if (options === false)
+        continue;
+
+      // Avoid any issues with _proto_
+      if (!plugins.hasOwnProperty(name))
+        continue;
+
+      this.plugins[name] = new plugin(this, options);
+    }
+  }
+};
+
+})();
